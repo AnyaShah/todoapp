2c1a3e7e33af4798e5544b4a24e06542c299349f
AnyaShah <pearlice6@gmail.com>
2020-08-05 22:07:41 +0500

    todo app

---
+++ app.js
@@ -0,0 +1,52 @@
+var list = document.getElementById("list");
+
+function addTodo(){
+    var todoitem = document.getElementById("todo-item");
+var li = document.createElement('li');
+var liText = document.createTextNode(todoitem.value);
+if ( todoitem.value == ""){
+    alert("Please add something");
+    return false;
+}
+li.appendChild(liText);
+var delBtn = document.createElement("img");
+delBtn.setAttribute("src","delete.png");
+delBtn.setAttribute("class","btn ")
+var delText = document.createTextNode("");
+delBtn.setAttribute("onClick","delItem(this)");
+delBtn.appendChild(delText);
+
+var editBtn = document.createElement("img")
+    editBtn.setAttribute("src","edit.png");
+    editBtn.setAttribute("class","btn")
+    var editText = document.createTextNode("");
+    editBtn.setAttribute("onclick", "editItem(this)");
+    editBtn.appendChild(editText);
+    li.appendChild(editBtn)
+    
+    console.log(li)
+li.appendChild(editBtn);
+li.appendChild(delBtn);
+
+
+list.appendChild(li);
+
+todoitem.value = "";
+}
+
+function delItem(e){
+    e.parentNode.remove();
+    console.log(e)
+
+}
+
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+    var editValue = prompt ("Edit your list",val)
+
+    e.parentNode.firstChild.nodeValue = editValue;
+    }
+    
+function delAll(){
+    list.innerHTML="";
+}
